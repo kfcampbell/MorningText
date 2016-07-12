50b8c4c37e0a9ae885cfe8dc116c309630b7fbf5
kfcampbell <keeg4n.campbell@gmail.com>
2016-07-12 13:41:17 -0700

    Pitching updates.

--- MorningText.py
+++ MorningText.py
@@ -21,9 +21,14 @@ if now.month < 10:
     month_string = "0" + month_string
 
 # construct the big url we need
-url_string = "http://gd2.mlb.com/components/game/mlb/year_" + year_string + "/month_" + month_string + "/day_" + day_string + "/master_scoreboard.json"
+# url_string = "http://gd2.mlb.com/components/game/mlb/year_" + year_string + "/month_" + month_string + "/day_" + day_string + "/master_scoreboard.json"
 
 # get the information
+# response = urllib2.urlopen(url_string)
+# data = json.load(response)
+
+# dummy practice for processing. remove later.
+url_string = "http://gd2.mlb.com/components/game/mlb/year_2016/month_07/day_15/master_scoreboard.json"
 response = urllib2.urlopen(url_string)
 data = json.load(response)
 
@@ -31,7 +36,38 @@ data = json.load(response)
 # print data["data"]
 data_one = data["data"]
 games = data_one["games"] # going to need a for loop here in the future to search for the M's games.
-print games["game"]
+game_one = games["game"]
+
+for game in game_one:
+    if game["away_team_name"] == "Mariners" or game["home_team_name"] == "Mariners":
+        print "M's are playing today!"
+
+    game_string = game["away_team_name"] + " at " + game["home_team_name"]
+    away_pitcher = game["away_probable_pitcher"]
+    home_pitcher = game["home_probable_pitcher"]
+    away_pitcher_stats = ""
+    home_pitcher_stats = ""
+    
+
+    # construct the pitcher names
+    
+    # now check to see if there's a pitcher announced yet
+    if not away_pitcher["last"]:
+        away_pitcher_name = "Unannounced"
+    else:
+        away_pitcher_name = away_pitcher["first"] + " " + away_pitcher["last"]
+        away_pitcher_stats = " (" + away_pitcher["s_wins"] + "-" + away_pitcher["s_losses"] + ", " + away_pitcher["s_era"] + ") "
+
+    if not home_pitcher["last"]:
+        home_pitcher_name = "Unannounced"
+    else:
+        home_pitcher_name = home_pitcher["first"] + " " + home_pitcher["last"]
+        home_pitcher_stats = " (" + home_pitcher["s_wins"] + "-" + home_pitcher["s_losses"] + ", " + home_pitcher["s_era"] + ") "
+
+
+    pitcher_string = "Pitching matchup: " + away_pitcher_name + away_pitcher_stats + " vs. " + home_pitcher_name + home_pitcher_stats
+    print game_string
+    print pitcher_string
 
 # construct the message. this is an example that needs to be changed to actual data once the All-Star break is over.
 home_pitcher = "Iwakuma"
--- readme.md
+++ readme.md
@@ -7,6 +7,7 @@ Will probably involve a cron job run each morning.
 # Future ideas:
     * M's starters and game time.
     * Weather forecast for day (hardcoded location?)
+    * Clean up into a class and make/call functions accordingly to reduce code bloat.
 
 # Useful Links
     * http://gd2.mlb.com/components/game/mlb/year_2016/month_07/day_17/master_scoreboard.json
